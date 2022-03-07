0cb739e7c19c25118089258ac459d2598cfc3d6a
Parshanl <>
2022-03-06 16:39:07 -0800

    testing other branch

--- prison-escape/src/main/java/com/group10/app/Entities/Test.java
+++ prison-escape/src/main/java/com/group10/app/Entities/Test.java
@@ -7,8 +7,6 @@ public class Test {
         System.out.println("hello");
         System.out.println("hello");
 
-        System.out.println("hello");
-        System.out.println("hello");
 
     }
 }
