1f4917fe9fc81a13f5492f74f8ad413ee006fda6
Parshanl <>
2022-03-08 12:30:39 -0800

    Implemented player movement

---
+++ prison-escape/src/main/java/com/group10/app/main/GameLauncher.java
@@ -0,0 +1,22 @@
+package com.group10.app.main;
+
+import javax.swing.*;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Prison Escape");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
