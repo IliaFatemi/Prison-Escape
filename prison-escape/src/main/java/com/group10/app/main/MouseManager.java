647247badbcad85e4a507cb8db1a1509a6aeeda9
Ilia Fatemi <ilia_fatemi@sfu.ca>
2022-03-13 23:25:00 -0700

    Added functionality for the game won menu

--- prison-escape/src/main/java/com/group10/app/main/MouseManager.java
+++ prison-escape/src/main/java/com/group10/app/main/MouseManager.java
@@ -4,12 +4,14 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 
+import com.group10.app.SavedData.SaveGame;
+import com.group10.app.entity.Inmate;
 import com.group10.app.main.GamePanel.STATE;
 
 public class MouseManager implements MouseListener{
 
     GamePanel gb;
-    
+    SaveGame saveGame;
 
     public MouseManager(GamePanel gb){
         this.gb = gb;
@@ -31,18 +33,20 @@ public class MouseManager implements MouseListener{
     public void mouseReleased(MouseEvent e) {
         int mouseX = e.getX();
         int mouseY = e.getY();
-        /* g2.drawImage(newGame,  gp.screenWidth/2-100, 100, 206, 70, null);
-        g2.drawImage(continue_, gp.screenWidth/2-100, 200, 206, 70, null);
-        g2.drawImage(quitGame, gp.screenWidth/2-100, 300, 206, 70, null);*/
 
         //Mouse control works only in main menu
-        if(GamePanel.state != STATE.GAME && GamePanel.state != STATE.PAUSED){
+        if(GamePanel.state != STATE.GAME && GamePanel.state != STATE.PAUSED && GamePanel.state != STATE.GAMEWON && GamePanel.state != STATE.GAMEOVER){
             MainMenuControls(mouseX, mouseY);
         }
 
         //Mouse control works only in pause menu
-        if(GamePanel.state != STATE.GAME && GamePanel.state != STATE.MENU){
+        if(GamePanel.state != STATE.GAME && GamePanel.state != STATE.MENU && GamePanel.state != STATE.GAMEWON && GamePanel.state != STATE.GAMEOVER){
             PauseMenuControls(mouseX, mouseY);
+        }
+
+        //Mouse control for game won menu
+        if(GamePanel.state != STATE.GAME && GamePanel.state != STATE.MENU && GamePanel.state != STATE.GAMEOVER && GamePanel.state != STATE.PAUSED){
+            GameWonMenuControls(mouseX, mouseY);
         }
         
     }
@@ -65,16 +69,23 @@ public class MouseManager implements MouseListener{
         g2.drawImage(newGame,  gp.screenWidth/2-103, gp.screenHeight/2 - 300, 206, 70, null);
         g2.drawImage(continue_, gp.screenWidth/2-103, gp.screenHeight/2 - 200, 206, 70, null);
         g2.drawImage(quitGame, gp.screenWidth/2-103, gp.screenHeight/2 - 100, 206, 70, null); */
+
         //new game button
         if(mouseX >= gb.screenWidth/2-103 && mouseX <= gb.screenWidth/2+(106)){
             if(mouseY >= gb.screenHeight/2-300 && mouseY <= gb.screenHeight/2-230){
                 GamePanel.state = GamePanel.STATE.GAME;
+                gb.inmate.resetInmate();
             }
         }
 
         //Continue button
         if(mouseX >= gb.screenWidth/2-103 && mouseX <= gb.screenWidth/2+(106)){
             if(mouseY >= gb.screenHeight/2 - 200 && mouseY <= gb.screenHeight/2 - 130){
+                GamePanel.state = STATE.GAME;
+                gb.inmate.setPos(gb.load.loadPlayerX(), gb.load.loadPlayerY());
+                gb.inmate.setTimer(gb.load.loadTimer());
+                gb.inmate.setScore(gb.load.loadScore());
+                gb.inmate.setNumKeys(gb.load.loadNumKeys());
             }
         }
 
@@ -105,7 +116,28 @@ public class MouseManager implements MouseListener{
         if(mouseX >= gb.screenWidth/2-103 && mouseX <= gb.screenWidth/2+(103)){
             if(mouseY >= gb.screenHeight/2 + 70 && mouseY <= gb.screenHeight/2 + 140){
                 GamePanel.state = STATE.MENU;
+                gb.saveGame.save(1,(int) gb.inmate.getX(),(int) gb.inmate.getY(), gb.inmate.getTimer(), gb.inmate.getScore(), gb.inmate.getNumKeys(), 0, 0);       
             }
         }
     }
+
+    public void GameWonMenuControls(int mouseX, int mouseY){
+        
+        //next level button
+        if(mouseX >= gb.screenWidth/2-103 && mouseX <= gb.screenWidth/2+(106)){
+            if(mouseY >= gb.screenHeight/2-130 && mouseY <= gb.screenHeight/2-60){
+                //System.out.println(mouseX + " "+ mouseY);
+            }
+        }
+
+        //return to main menu button
+        if(mouseX >= gb.screenWidth/2-103 && mouseX <= gb.screenWidth/2+(106)){
+            if(mouseY >= gb.screenHeight/2 - 30 && mouseY <= gb.screenHeight/2 + 40){
+                gb.saveGame.save(1,(int) gb.inmate.getX(),(int) gb.inmate.getY(), gb.inmate.getTimer(), gb.inmate.getScore(), gb.inmate.getNumKeys(), 0, 0);   
+                GamePanel.state = STATE.MENU;
+            }
+        }
+    }
+
+
 }
