544e1927a9cbbea09218192dddafb53fa98ddd0c
Ilia Fatemi <ilia_fatemi@sfu.ca>
2022-03-18 23:56:14 -0700

    Walls have been polished

--- Prison_Escape/src/main/java/com/group10/app/objects/TileManager.java
+++ Prison_Escape/src/main/java/com/group10/app/objects/TileManager.java
@@ -19,7 +19,7 @@ public class TileManager {
 
     public TileManager(GamePanel gp){
         this.gp = gp;
-        tile = new Tiles[31];
+        tile = new Tiles[36];
         mapTileNum = new int[gp.screenColNumber][gp.screenRowNumber];
         registerImage();
     }
@@ -59,6 +59,11 @@ public class TileManager {
         setup(28, "cornerTopRight", true);
         setup(29, "cornerBottomLeft", true);
         setup(30, "cornerBottomRight", true);
+        setup(31, "TUp", true);
+        setup(32, "TDown", true);
+        setup(33, "TRight", true);
+        setup(34, "TLeft", true);
+        setup(35, "4WayWall", true);
     }
 
     public void setup(int index, String imageName, boolean collision) {
