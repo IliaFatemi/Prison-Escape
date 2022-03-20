8c2c48d8450baef26279f987d5b32888be9b88ab
junyel <junye_lu@sfu.ca>
2022-03-19 21:09:36 -0700

    javaDoc for Sound

--- Prison_Escape/src/main/java/com/group10/app/main/Sound.java
+++ Prison_Escape/src/main/java/com/group10/app/main/Sound.java
@@ -5,10 +5,22 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.net.URL;
 
+/**
+ * This is class for music and sound effects
+ */
+
 public class Sound {
     Clip clip;
-    URL[] soundURL = new URL[30];
+    URL[] soundURL = new URL[10];
 
+    /**
+     * This is the constructor for the Sound class.
+     *
+     * <p>
+     *     It is in charge of registering the sound directories.
+     * </p>
+     *
+     */
     public Sound() {
 
         soundURL[0] = getClass().getResource("/sound/bgm.wav");
@@ -16,13 +28,20 @@ public class Sound {
         soundURL[2] = getClass().getResource("/sound/getTimer.wav");
         soundURL[3] = getClass().getResource("/sound/getChicken.wav");
         soundURL[4] = getClass().getResource("/sound/getTrap.wav");
-
     }
 
+    /**
+     * Get the sound that need to play
+     *
+     * <p>
+     *     This method will get the sound from the soundURL list by passing the index i.
+     * </p>
+     *
+     * @param i index of sound in soundURL list
+     */
     public void setFile(int i) {
 
         try {
-
             AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
             clip = AudioSystem.getClip();
             clip.open(ais);
@@ -30,14 +49,23 @@ public class Sound {
         }catch (Exception ignored){}
     }
 
+    /**
+     * Play the current sound.
+     */
     public void play() {
         clip.start();
     }
 
+    /**
+     * Loop the current sound.
+     */
     public void loop() {
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
+    /**
+     * Stop the current sound.
+     */
     public void stop() {
         clip.stop();
     }
