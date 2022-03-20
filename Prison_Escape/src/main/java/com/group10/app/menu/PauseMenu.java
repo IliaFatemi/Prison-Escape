8fa192e90149e1d4a86051e81afb4465bc88a809
Ilia Fatemi <ilia_fatemi@sfu.ca>
2022-03-19 20:23:37 -0700

    Added javadocs to the menu package and removed unused variables.

--- Prison_Escape/src/main/java/com/group10/app/menu/PauseMenu.java
+++ Prison_Escape/src/main/java/com/group10/app/menu/PauseMenu.java
@@ -7,26 +7,39 @@ import java.io.IOException;
 import java.util.Objects;
 
 import com.group10.app.main.GamePanel;
-import com.group10.app.main.KeyManager;
 
+/**
+ * PauseMenu will create an object for the pause menue screen.
+ * @author Ilia Fatemi
+ */
 public class PauseMenu {
-    GamePanel gp;
-    KeyManager keyH;
 
-    //Screen graphics 
+    /**
+     * Game panel will be used to access it's contacts
+     */
+    GamePanel gp;
+
+    /**
+     * resume: will register the resume button
+     * saveGame: will register the save game button
+     * returnMenu: will register the return to main menu button
+     * pauseTemplate: will register the background image
+     */ 
     public BufferedImage resume, saveGame, returnMenu, pauseTemplate;
 
-    public PauseMenu(GamePanel gp, KeyManager keyH){
+    /**
+     * <p>The contructor for PauseMenu will setup the registered images</p>
+     * @param gp the GamePanel object
+     */
+    public PauseMenu(GamePanel gp){
         this.gp = gp;
-        this.keyH = keyH;
-        getPausedGraphics();
+        gregisterPausedGraphics();
     }
 
-    public void resume(){
-        keyH.pressedEscape = false;
-    }
-
-    public void getPausedGraphics(){
+    /**
+     * <p>Registering the images for the background, resume button, save game button, return to main menu button.</p>
+     */
+    public void gregisterPausedGraphics(){
         try{
             resume = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/menu/resumeBtn.png")));
             saveGame = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/menu/saveGameBtn.png")));
@@ -37,6 +50,10 @@ public class PauseMenu {
         }
     }
 
+    /**
+     * <p>Display the graphics onto the screen. This method will display the baground image, resume button, save game button, return to main menu button.</p>
+     * @param g2 using the Graphics2D to draw the registered images onto the display
+     */
     public void renderPauseMenu(Graphics2D g2){
         g2.drawImage(pauseTemplate, gp.screenWidth/2-350, gp.screenHeight/2-250, 700, 500, null);
         g2.drawImage(resume, gp.screenWidth/2 - 103, gp.screenHeight/2 - 130, 206, 70, null);
