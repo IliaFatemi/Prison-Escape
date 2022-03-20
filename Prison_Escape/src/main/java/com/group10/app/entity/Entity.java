c82c254c861e94dadaedc1f5ca566d53e96e845b
junyel <junye_lu@sfu.ca>
2022-03-19 23:33:39 -0700

    javaDoc GamePanel paintComponent, and all three of music

--- Prison_Escape/src/main/java/com/group10/app/entity/Entity.java
+++ Prison_Escape/src/main/java/com/group10/app/entity/Entity.java
@@ -73,6 +73,7 @@ public class Entity {
 
                 if (doorLightly < i) {
                     changeAlpha(g2, 0.5f);
+                    System.out.println("GAME_LEVEL is " + GamePanel.GAME_LEVEL);
                 }
                 else {
                     changeAlpha(g2, 1f);
