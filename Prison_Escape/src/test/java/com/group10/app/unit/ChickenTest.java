c0e1b48976b1787b7698ccff7417c6b34ad538ad
Parshanl <>
2022-03-24 14:14:53 -0700

    created first unit test

---
+++ Prison_Escape/src/test/java/com/group10/app/unit/ChickenTest.java
@@ -0,0 +1,12 @@
+package com.group10.app.unit;
+
+import com.group10.app.objects.Chicken;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class ChickenTest {
+    @Test
+    public void testChicken(){
+        
+    }
+}
