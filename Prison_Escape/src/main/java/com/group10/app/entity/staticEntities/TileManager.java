07e9fb3a250dc89f7efce07cf75e333e4e95ab25
Ilia Fatemi <ilia_fatemi@sfu.ca>
2022-04-02 22:23:33 -0700

    Level 2 redesigned

--- Prison_Escape/src/main/java/com/group10/app/entity/staticEntities/TileManager.java
+++ Prison_Escape/src/main/java/com/group10/app/entity/staticEntities/TileManager.java
@@ -42,7 +42,7 @@ public class TileManager {
      */
     public TileManager(GamePanel gp){
         this.gp = gp;
-        tile = new Tiles[79];
+        tile = new Tiles[88];
         mapTileNum = new int[gp.screenColNumber][gp.screenRowNumber];
         registerImage();
     }
