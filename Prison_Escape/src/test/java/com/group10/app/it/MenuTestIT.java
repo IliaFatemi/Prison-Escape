8a018058316dafbc96e1cfbadc583bfe5cf6e5b0
junyel <junye_lu@sfu.ca>
2022-04-03 18:58:10 -0700

    add the rest of menu test

--- Prison_Escape/src/test/java/com/group10/app/it/MenuTestIT.java
+++ Prison_Escape/src/test/java/com/group10/app/it/MenuTestIT.java
@@ -13,8 +13,7 @@ import org.junit.jupiter.api.Test;
 import java.awt.event.KeyEvent;
 import java.security.Key;
 
-import static com.group10.app.main.GameStates.GAME;
-import static com.group10.app.main.GameStates.PAUSED;
+import static com.group10.app.main.GameStates.*;
 import static java.awt.event.KeyEvent.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -49,6 +48,37 @@ public class MenuTestIT {
         gp.setState(PAUSED);
         assertEquals(PAUSED, gp.getState());
 
+        gp.mouseK.PauseMenuControls(700,333);
+        assertEquals(GameStates.GAME, gp.getState());
 
+        gp.setState(PAUSED);
+        gp.mouseK.PauseMenuControls(700,533);
+        assertEquals(GameStates.MENU, gp.getState());
+    }
+
+    @Test
+    public void gameOverMenuButton(){
+        gp.setState(GAMEOVER);
+        assertEquals(GAMEOVER, gp.getState());
+
+        gp.mouseK.GameOverMenuControls(700,333);
+        assertEquals(GameStates.GAME, gp.getState());
+
+        gp.setState(GAMEOVER);
+        gp.mouseK.GameOverMenuControls(700,433);
+        assertEquals(GameStates.MENU, gp.getState());
+    }
+
+    @Test
+    public void gameWonMenuButton(){
+        gp.setState(GAMEWON);
+        assertEquals(GAMEWON, gp.getState());
+
+        gp.mouseK.GameWonMenuControls(700,333);
+        assertEquals(GameStates.GAME, gp.getState());
+
+        gp.setState(GAMEWON);
+        gp.mouseK.GameWonMenuControls(700,433);
+        assertEquals(GameStates.MENU, gp.getState());
     }
 }
