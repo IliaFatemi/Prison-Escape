421e8f1c3510fe2741a3e97344314bced43bf7f4
junyel <junye_lu@sfu.ca>
2022-04-03 18:38:05 -0700

    add main menu test and fix pressed twice esc

---
+++ Prison_Escape/src/test/java/com/group10/app/it/MenuTestIT.java
@@ -0,0 +1,54 @@
+package com.group10.app.it;
+
+import com.group10.app.entity.nonStaticEntities.Inmate;
+import com.group10.app.entity.staticEntities.Chicken;
+import com.group10.app.main.GamePanel;
+import com.group10.app.main.GameStates;
+import com.group10.app.main.KeyManager;
+import com.group10.app.main.MouseManager;
+import com.group10.app.menu.Menu;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.event.KeyEvent;
+import java.security.Key;
+
+import static com.group10.app.main.GameStates.GAME;
+import static com.group10.app.main.GameStates.PAUSED;
+import static java.awt.event.KeyEvent.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MenuTestIT {
+    private GamePanel gp;
+
+    @BeforeEach
+    void setUp(){
+        gp = new GamePanel();
+        gp.mouseK = new MouseManager(gp);
+
+    }
+
+    @Test
+    public void mainMenuButton(){
+        assertEquals(GameStates.MENU, gp.getState());
+
+        gp.mouseK.MainMenuControls(700,168);
+        assertEquals(GameStates.GAME, gp.getState());
+
+        gp.setState(GameStates.MENU);
+        gp.mouseK.MainMenuControls(700,268);
+        assertEquals(GameStates.GAME, gp.getState());
+
+        gp.setState(GameStates.MENU);
+//        gp.mouseK.MainMenuControls(700,368);
+//        assertEquals(null, gp);
+    }
+
+    @Test
+    public void pauseMenuButton(){
+        gp.setState(PAUSED);
+        assertEquals(PAUSED, gp.getState());
+
+
+    }
+}
